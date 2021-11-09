080454f554e8cd7ce3e02951f786adb455061e0a
=Albs <=driton.koskovik@icloud.com>
2021-11-09 18:52:01 +0100

    ok

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,16 +1,19 @@
+
 const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
-
 const newYears = "1 Jan 2022";
 
 function countdown() {
+
+
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
    //console.log(newYearsDate - currentDate) / 1000;
    const totalSeconds = (newYearsDate - currentDate) / 1000;
    const seconds = Math.floor(totalSeconds)  % 60;
+
    const days = Math.floor(totalSeconds / 3600 /24);
    const hours = Math.floor(totalSeconds / 3600) % 24;
    const mins = (Math.floor(totalSeconds / 60) % 60);
